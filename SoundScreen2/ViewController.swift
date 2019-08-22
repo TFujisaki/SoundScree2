92ad04c281cafe463c88688dcfa89b4653d7d96e
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2019-08-23 01:44:49 +0900

    Initial Commit

---
+++ SoundScreen2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundScreen2
+//
+//  Created by 藤崎 達也 on 2019/08/23.
+//  Copyright © 2019年 藤崎 達也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
