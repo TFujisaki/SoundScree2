b7012c43e58f777d99ea6e6a103594c2445dc538
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2019-08-24 19:18:12 +0900

    音を止める

--- SoundScreen2/ViewController.swift
+++ SoundScreen2/ViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 import CoreMotion
-import AudioToolbox
+// import AudioToolbox
 import AVFoundation
-import Foundation
+//import Foundation
 
 class ViewController: UIViewController {
     
@@ -22,92 +22,90 @@ class ViewController: UIViewController {
 
     // Constant
     let updateInterval = 0.05
-    let velocityXThreshold = 4.0
+    let velocityXThreshold = 3.5
+    let volumeSetting: Float = 1.0
     
     // Image File name definition
     let imageFileName = "Artboard.jpg"
     
+    // Sound File name definition
+    let soundFileName = "take2"
+    let fileExtension = "m4a"
     
     var accelerometerX = 0.0
     var accXTemp = 0.0
     var velocityX = 0.0
+    
+    // Flags
+    var onoffFlag:Int = 0
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        
-       // while true {
-        
-            if motionManager.isAccelerometerAvailable {
-                // intervalの設定 [sec]
-                motionManager.accelerometerUpdateInterval = updateInterval
+        if motionManager.isAccelerometerAvailable {
+            // intervalの設定 [sec]
+            motionManager.accelerometerUpdateInterval = updateInterval
             
-                // センサー値の取得開始
-                motionManager.startAccelerometerUpdates(
-                    to: OperationQueue.current!,
-                    withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
-                        self.outputAccelData(acceleration: accelData!.acceleration)
-                })
-            }
-            
-        // }
+            // センサー値の取得開始
+            motionManager.startAccelerometerUpdates(
+                to: OperationQueue.current!,
+                withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
+                    self.outputAccelData(acceleration: accelData!.acceleration)
+            })
+        }
     }
     
     func outputAccelData(acceleration: CMAcceleration) {
         // 加速度センサー [G]
         accelerometerX = acceleration.x
-        
-        // accelerometerX = accXTemp
-        
-        accXTemp = accelerometerX - accXTemp
-        
-        // print("1: \(accXTemp)")
-        
-        accXTemp = accelerometerX
-        
-        // print("2: \(accXTemp)")
-        
         // 速度の計算
         velocityX = accelerometerX * accelerometerX
-        print("V: \(velocityX)")
         
         if velocityX > velocityXThreshold {
-            print(" true ")
-
-            // バンドルした画像ファイルを読み込み
-            let image = UIImage(named: imageFileName)
+            onoffFlag += 1
             
-            // Image Viewに画像を設定
-            instructionImage.image = image
+            if onoffFlag == 1 {
+                print(" true ")
             
+                let url = Bundle.main.url(forResource: soundFileName, withExtension: fileExtension)
             
-            let url = Bundle.main.url(forResource: "test", withExtension: "m4a")
-            
-            do {
-                try audioPlayer = AVAudioPlayer(contentsOf: url!)
+                do {
+                    try audioPlayer = AVAudioPlayer(contentsOf: url!)
                 
-                //音楽をバッファに読み込んでおく
-                audioPlayer.prepareToPlay()
-            } catch {
-                print("path   ", url!)
-                print(error)
-            }
+                    //サウンドをバッファに読み込んでおく
+                    audioPlayer.prepareToPlay()
+                } catch {
+                    print("url   ", url!)
+                    print(error)
+                }
             
-            audioPlayer.play()
+                audioPlayer.volume = volumeSetting
+                audioPlayer.play()
+                
+            } else if onoffFlag == 2 {
+                audioPlayer.stop()
+            }
             
         } else {
             print(" false ")
         }
+        stopAccelerometer()
     }
     
     // センサー取得を止める場合
-    func stopAccelerometer(){
+    func stopAccelerometer() {
         if (motionManager.isAccelerometerActive) {
             motionManager.stopAccelerometerUpdates()
         }
+    }
+    
+    func dummy() {
+        // バンドルした画像ファイルを読み込み
+        // let image = UIImage(named: imageFileName)
+        // Image Viewに画像を設定
+        // instructionImage.image = image
     }
 
 
