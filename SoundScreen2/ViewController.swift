4986cfc72e45bbf4180abeb2b367f654b38f6d9c
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2019-08-27 13:49:27 +0900

    加速度センサーをやめて、ボタン操作にした

--- SoundScreen2/ViewController.swift
+++ SoundScreen2/ViewController.swift
@@ -7,118 +7,59 @@
 //
 
 import UIKit
-import CoreMotion
-// import AudioToolbox
 import AVFoundation
-//import Foundation
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var instructionImage: UIImageView!
+    @IBOutlet weak var soundOnButton: UIButton!
+    @IBOutlet weak var soundOffButton: UIButton!
     
-    // MotionManager
-    let motionManager = CMMotionManager()
+    // let motionManager = CMMotionManager()
     var audioPlayer: AVAudioPlayer!
 
     // Constant
-    let updateInterval = 0.01
-    let velocityXThreshold = 5.0
     let volumeSetting: Float = 1.0
-    let startSound = 1
-    let stopSound = 2
-    
-    // Image File name definition
-    let imageFileName = "Artboard.jpg"
-    
+  
     // Sound File name definition
     let soundFileName = "testSound"
     let fileExtension = "wav"
-    
-    var accelerometerX = 0.0
-    var accXTemp = 0.0
-    var velocityX = 0.0
-    
-    // Flags
-    var onoffFlag:Int = 0
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        if motionManager.isAccelerometerAvailable {
-            // intervalの設定 [sec]
-            motionManager.accelerometerUpdateInterval = updateInterval
-            
-            // センサー値の取得開始
-            motionManager.startAccelerometerUpdates(
-                to: OperationQueue.current!,
-                withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
-                    self.outputAccelData(acceleration: accelData!.acceleration)
-            })
-        }
     }
     
-    func outputAccelData(acceleration: CMAcceleration) {
-        // 加速度センサー [G]
-        accelerometerX = acceleration.x
-        // 速度の計算
-        velocityX = accelerometerX * accelerometerX
+    
+    func soundOn() {
+        let url = Bundle.main.url(forResource: soundFileName, withExtension: fileExtension)
         
-        if velocityX > velocityXThreshold {
-            onoffFlag += 1
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: url!)
             
-            print("flag : \(onoffFlag)")
-            
-            switch onoffFlag {
-            case startSound :
-                print(" true ")
-            
-                let url = Bundle.main.url(forResource: soundFileName, withExtension: fileExtension)
-            
-                do {
-                    try audioPlayer = AVAudioPlayer(contentsOf: url!)
-                
-                    //サウンドをバッファに読み込んでおく
-                    audioPlayer.prepareToPlay()
-                } catch {
-                    print("url   ", url!)
-                    print(error)
-                }
-            
-                audioPlayer.volume = volumeSetting
-                audioPlayer.play()
-                break
-                
-            case stopSound:
-                audioPlayer.stop()
-                break
-            
-            default:
-                onoffFlag = 0
-                break
-            }
-           
-        } else {
-            print(" false ")
+            //サウンドをバッファに読み込んでおく
+            audioPlayer.prepareToPlay()
+        } catch {
+            print("url   ", url!)
+            print(error)
         }
         
+        audioPlayer.volume = volumeSetting
+        audioPlayer.play()
     }
     
-    // センサー取得を止める場合
-    func stopAccelerometer() {
-        if (motionManager.isAccelerometerActive) {
-            motionManager.stopAccelerometerUpdates()
-        }
+    func soundOff() {
+        audioPlayer.stop()
     }
     
-    func dummy() {
-        // バンドルした画像ファイルを読み込み
-        // let image = UIImage(named: imageFileName)
-        // Image Viewに画像を設定
-        // instructionImage.image = image
+    
+    @IBAction func soiundOnButtonTapped(_ sender: UIButton) {
+        soundOn()
     }
-
+    
+    @IBAction func soundOffButtonTapped(_ sender: UIButton) {
+        soundOff()
+    }
 
 }
 
